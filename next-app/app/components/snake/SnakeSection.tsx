ecc83a6870f8901b21fcb0befae70730712440a7
Erwan <erwan.coubret@gmail.com>
2025-12-05 01:45:03 +0100

    fix: inused deps

--- next-app/app/components/snake/SnakeSection.tsx
+++ next-app/app/components/snake/SnakeSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { use, useEffect, useState } from "react";
+import { useState } from "react";
 import { FaArrowRight } from "react-icons/fa6";
 
 export default function SnakeSection() {

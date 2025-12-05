94ea0b3acf9ca54e8b53e311e809d5b4d6041505
Erwan <erwan.coubret@gmail.com>
2025-12-05 01:05:13 +0100

    fix: pardon nathan

--- next-app/app/components/snake/SnakeSection.tsx
+++ next-app/app/components/snake/SnakeSection.tsx
@@ -26,7 +26,7 @@ export default function SnakeSection() {
     <div id="snake" className="w-full h-fit pb-1 px-1 lg:pb-3 lg:px-3">
       <div className="w-full h-full min-h-[70vh] flex flex-col gap-5 items-center justify-center bg-slate-100 py-10 px-4 rounded xl:rounded-xl relative overflow-hidden">
         <p className="text-center text-3xl text-purple-400 font-bold">
-          Snake face aux dilèmmes !!! 🐍🐍🐍
+          Snake face aux dilemmes !!! 🐍🐍🐍
         </p>
 
         <p className="text-center text-slate-500 font-medium max-w-2xl">
